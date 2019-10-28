963a8557792fe53d61651061d5f471171132a121
zboss2u <snowboardallday15@gmail.com>
2019-10-28 12:06:30 -0600

    Added relevent comments

--- simulation_1.py
+++ simulation_1.py
@@ -45,10 +45,8 @@ if __name__ == '__main__':
     for t in thread_L:
         t.start()
     
-    #create some send events    
-    #for i in range(3):
-       #client.udt_send(2, 'This is a longer string for simulation_1. This string is too long for this MTU %d' % i)
-       #client.udt_send(2, 'This is short string %d' % i)
+    #create a send event that sends a string longer than the mtu
+    #As a result it must segment the string into sendable lengths
     
     message = 'This is a longer string for simulation_1. This string is too long for this MTU'
     segMessage = []
